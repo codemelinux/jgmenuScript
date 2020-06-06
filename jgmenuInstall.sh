9721a1cc2e670217215f7196fe29181811f633f0
cml <cml@pop-os.localdomain>
2020-06-06 16:13:43 +0800

    First Commit

---
+++ jgmenuInstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "Installing important libraries"
+#### Installing libraries ###
+
+sudo apt update && sudo apt install -y libx11-xcb-dev libxrandr-dev libghc-cairo-dev libghc-pango-dev librsvg2-dev libmenu-cache-dev git
+echo "Successfully installed."
+echo ""
+
+cd ~/Downloads && git clone https://github.com/johanmalm/jgmenu.git
+echo ""
+
+cd jgmenu
+./configure --prefix=/usr
+make
+sudo make install && jgmenu_run init
+echo "Successfully installed."
+
+echo ""
+echo "Copying CodeMe.Linux jgmenu config"
+cp -r ~/jgmenuScript/customJgmenuConfig/jgmenu/ ~/.config/
+echo "process complete.... :)"
+
